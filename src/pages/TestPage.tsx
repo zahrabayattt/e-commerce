d698dcca05e398205348774bb056be7d2897a560
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-06-10 13:57:56 +0330

    feat: :sparkles: Create a test page

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,12 @@
+// Don't remove
+// This is a test page for testing components and other pages
+
+const TestPage = () => {
+  return (
+    <>
+      <h1>صفحه تست</h1>
+    </>
+  );
+};
+
+export default TestPage;
