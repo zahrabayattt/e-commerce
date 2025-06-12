42bdbd4bdac4e37baaf30dfea78606e06474f741
ZahraKiani <zahra.kiani7776@gmail.com>
2025-06-12 22:24:31 +0330

    feat: :sparkles: Implement  product page In the pages folder
    #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { Button } from "./components/ui/button";
-import ProductCard from "./components/ui/productCard";
 
 function App() {
   return (
     <>
       <Button>کلیک</Button>
-      <ProductCard
-        productId={2}
-        productTitle="ژل شست و شو مناسب پوست چرب و مختلط"
-        productImage={'./src/assets/images/auth-light.png'}
-        price={500000}
-        componentSize="small"
-      />
-      <ProductCard
-        productId={2}
-        productTitle="ژل شست و شو مناسب پوست چرب"
-        productImage={'./src/assets/images/auth-light.png'}
-        price={500000}
-        componentSize="large"
-      />
     </>
   );
 }
