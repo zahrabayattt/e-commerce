6b9dcf7777bb84832b3cdd0ecd0c4aa037717a9e
Zahra <zahrabayat367@gmail.com>
2025-06-13 20:40:08 +0330

    feat: :sparkles: Create first page of ShoppingProgress

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -4,14 +4,16 @@ export interface IStepper {
 const steps = ['ورود', 'آدرس', 'خلاصه خرید'];
 const Stepper = ({ currentStep }: IStepper) => {
   return (
-    <div className="flex justify-center items-center gap-4">
+    <div className="flex justify-center items-center gap-5">
       {steps.map((step, index) => (
         <div key={index} className="flex items-center gap-2">
           <div className="flex flex-col items-center gap-2">
-            <p className={`text-[#22C55E] ${index >= currentStep ? 'text-black' : ''}`}>{step}</p>
+            <p className={`text-[#22C55E] text-sm ${index >= currentStep ? 'text-black' : ''}`}>
+              {step}
+            </p>
             {index < currentStep && <input type="checkbox" checked readOnly className="checkbox" />}
           </div>
-          {index < steps.length - 1 && <div className="w-40 h-0.5 bg-[#22C55E] mx-2" />}
+          {index < steps.length - 1 && <div className="w-30 h-0.5 bg-[#22C55E] mx-2" />}
         </div>
       ))}
     </div>
