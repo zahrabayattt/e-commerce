c915b75b44a561475a31e0c694d62e36f90daf55
Ali <beiramiali1380@gmail.com>
2025-07-03 21:29:33 +0330

    dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { RouterProvider } from 'react-router';
-import router from './router';
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "./components/mode-toggle"
+
+//import { RouterProvider } from 'react-router';
+//import router from './router';
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle/>
+    </ThemeProvider>
+    //<RouterProvider router={router} />
+
+  )
+  
 };
 
 export default App;
