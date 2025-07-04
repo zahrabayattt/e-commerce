936bd21447c7ea4634efa79942cdc83123b41893
javadi <javadi89s@gmail.com>
2025-07-04 18:21:19 +0330

    feat: :sparkles: created cart's reset & added persist middleware
    #11

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export interface CartItem {
   productId: number;
@@ -14,30 +15,39 @@ interface CartState {
   addToCart: (item: CartItem) => void;
   removeFromCart: (productId: number) => void;
   updateQuantity: (productId: number, quantity: number) => void;
+  clearCart: () => void
 }
 
-export const useCartStore = create<CartState>((set) => ({
-  cartItems: [],
-  addToCart: (item) =>
-    set((state) => {
-      const existingCartItems = state.cartItems.find((i) => i.productId === item.productId);
-      if (existingCartItems) {
-        return {
-          cartItems: state.cartItems.map((i) =>
-            i.productId === item.productId ? { ...i, quantity: i.quantity + item.quantity } : i
+export const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      cartItems: [],
+      addToCart: (item) => {
+        const existingItem = get().cartItems.find((i) => i.productId === item.productId);
+        if (existingItem) {
+          set({
+            cartItems: get().cartItems.map((i) =>
+              i.productId === item.productId ? { ...i, quantity: i.quantity + item.quantity } : i
+            ),
+          });
+        } else {
+          set({ cartItems: [...get().cartItems, item] });
+        }
+      },
+      removeFromCart: (productId) =>
+        set({
+          cartItems: get().cartItems.filter((item) => item.productId !== productId),
+        }),
+      updateQuantity: (productId, quantity) =>
+        set({
+          cartItems: get().cartItems.map((item) =>
+            item.productId === productId ? { ...item, quantity } : item
           ),
-        };
-      }
-      return { cartItems: [...state.cartItems, item] };
+        }),
+        clearCart: () => set({cartItems: []}),
     }),
-  removeFromCart: (productId) =>
-    set((state) => ({
-      cartItems: state.cartItems.filter((item) => item.productId !== productId),
-    })),
-  updateQuantity: (productId, quantity) =>
-    set((state) => ({
-      cartItems: state.cartItems.map((item) =>
-        item.productId === productId ? { ...item, quantity } : item
-      ),
-    })),
-}));
+    {
+      name: 'cart-storage', 
+    }
+  )
+);
