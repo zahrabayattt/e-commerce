87e20c8599b18a63f0340099d1049e95de914889
ZahraKiani <zahra.kiani7776@gmail.com>
2025-07-04 20:59:22 +0330

    feat: :sparkles: change product store to product and products hook

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,13 @@
+import { axiosInstance } from '@/lib/utils';
+import type { ProductModel } from '@/types/product.model';
+import { useQuery } from '@tanstack/react-query';
+
+
+export const useProduct = (id: string) => {
+ return useQuery({
+    queryKey: ['products', id],
+    queryFn: () => axiosInstance.get<ProductModel>(`/products/${id}`).then((res) => res.data),
+    gcTime: 10 * 60 * 1000,
+    staleTime: 10 * 60 * 1000,
+  });
+};
