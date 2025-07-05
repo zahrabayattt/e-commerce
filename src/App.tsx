2ee453b3e5110c541a07f2cf3a2dad1eb9dfbdfe
Zahra <zahrabayat367@gmail.com>
2025-07-05 21:51:29 +0330

    revert: :fire: remove dark mode feature (PR #72)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from "./components/mode-toggle"
-
-//import { RouterProvider } from 'react-router';
-//import router from './router';
+import { RouterProvider } from 'react-router';
+import router from './router';
 const App = () => {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle/>
-    </ThemeProvider>
-    //<RouterProvider router={router} />
-
-  )
-  
+  return <RouterProvider router={router} />;
 };
 
 export default App;
