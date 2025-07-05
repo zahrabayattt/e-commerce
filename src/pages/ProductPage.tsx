0c4064ff0e551533b740754f6f2d4f8eb80681a8
ZahraKiani <zahra.kiani7776@gmail.com>
2025-07-05 12:10:20 +0330

    feat: :hammer: create useMiladiToShamsi for convert date

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -2,6 +2,7 @@ import { Select,SelectContent,SelectItem,SelectTrigger,SelectValue,} from '@/com
 import { Button } from '@/components/ui/button';
 import { useParams } from 'react-router-dom';
 import { useProduct } from '@/hooks/useProduct';
+import useMiladiToShamsi from '@/hooks/useMiladiToShamsi';
 
 const ProductPage = () => {
 
@@ -51,7 +52,7 @@ const { id } = useParams();
                 <path d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
               </svg>
 
-              <p>{product.updatedAt}زمان بروزرسانی :</p>
+              <p>زمان به روز رسانی:{useMiladiToShamsi(product.updatedAt)}</p>
             </div>
 
             <div className="flex flex-row gap-2">
