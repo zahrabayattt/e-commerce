6ce7bbe851124e22db00b993da661710e8d8b1ff
moazame <moazame.abdolrezaei80@gmail.com>
2025-07-05 20:41:49 +0330

    feat: :sparkles: api implemented in ShopPage

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -3,7 +3,7 @@ import useFavoriteStore from "@/store/useFavoriteStore";
 const FavoritePage = () => {
   const favorites = useFavoriteStore((state) => state.favorites);
   return (
-    <div className="mr-30 grid grid-cols-3  ">
+    <div className="mr-30 grid grid-cols-3 gap-18 ">
         
         {favorites.slice(0,20).map((p)=>(
           <ProductCard key={p._id} product={p} 
--- src/types/product.model.ts
+++ src/types/product.model.ts
@@ -7,13 +7,19 @@ export type ReviewModel = {
   createdAt: Date;
   updatedAt: Date;
 };
+type category={
+  _id :string,
+  name :string,
+  __v:number,
+
+}
 
 export type ProductModel = {
   _id: string;
   name: string;
   image: string;
   quantity: number;
-  category: string;
+  category: category;
   description: string;
   rating: number;
   numReviews: number;
