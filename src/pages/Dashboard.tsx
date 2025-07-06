d4d6f60d2102434a610f109e98190311e999b844
Yasin <yasinsanjari7@gmail.com>
2025-07-06 08:21:22 +0330

    feat: :sparkles: completed dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,29 +1,44 @@
 import SalesChart from '@/components/SalesChart';
 import StaticsCard from '@/components/StaticsCard';
+import useGetOrders from '@/hooks/use-get-orders';
+import usePersianNumbers from '@/hooks/use-persian-numbers';
+import useSales from '@/hooks/use-sales';
 import useSalesByDate from '@/hooks/use-sales-by-date';
 import useUsers from '@/hooks/use-users';
 import { miladiToShamsi } from '@/lib/miladiToShamsi';
 const Dashboard = () => {
   const { data: users } = useUsers();
   const { data } = useSalesByDate();
+  const { data: orders } = useGetOrders(true);
+  const { data: sales } = useSales();
+
+  const toPersianNumbers = usePersianNumbers();
   const chartData =
     data
       ?.slice()
-      .sort((a, b) => new Date(b._id).getTime() - new Date(a._id).getTime()) // sort by actual date
+      .sort((a, b) => new Date(b._id).getTime() - new Date(a._id).getTime())
       .map((item) => ({
-        date: miladiToShamsi(item._id), // format after sorting
+        date: miladiToShamsi(item._id),
         sales: item.totalSales,
       })) || [];
 
   return (
     <div className="flex flex-col gap-16 md:pt-12 pt-12">
       <div className="flex md:gap-32 gap-8 mx-auto">
-        <StaticsCard title="فروش کل" value="0 تومان"></StaticsCard>
+        <StaticsCard
+          title="فروش کل"
+          value={
+            sales ? toPersianNumbers(sales.totalSales.toLocaleString().toString()) + ' تومان' : '۰'
+          }
+        ></StaticsCard>
         <StaticsCard
           title="مشتری ها"
-          value={Array.isArray(users) ? users.length.toString() : '0'}
+          value={Array.isArray(users) ? toPersianNumbers(users.length.toString()) : '۰'}
         ></StaticsCard>
-        <StaticsCard title="سفارشات" value="100"></StaticsCard>
+        <StaticsCard
+          title="سفارشات"
+          value={Array.isArray(orders) ? toPersianNumbers(orders.length.toString()) : '۰'}
+        ></StaticsCard>
       </div>
       <div>
         <SalesChart data={chartData}></SalesChart>
--- src/types/sales.model.ts
+++ src/types/sales.model.ts
@@ -2,3 +2,7 @@ export type SalesByDate = {
   _id: string;
   totalSales: number;
 };
+
+export type Sales = {
+  totalSales: number;
+};
