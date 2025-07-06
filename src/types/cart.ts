387e85b560e0bbc0f5b77e2fa6491d15fa843fb3
javadi <javadi89s@gmail.com>
2025-07-06 17:10:58 +0330

    refactor: :art: seperating types and moving them to type files

--- src/types/cart.ts
+++
@@ -1,8 +0,0 @@
-export interface ICartItem {
-  productId: number;
-  productTitle: string;
-  productImage: string;
-  productPrice: number;
-  productQuantity: number;
-  productTotalPrice: number;
-}
