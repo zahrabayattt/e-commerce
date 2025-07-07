00aef499d0b6193f0438c6a6f2159332341295ba
javadi <javadi89s@gmail.com>
2025-07-07 18:25:43 +0330

    feat: :sparkles: displaying cart quantity based on stock
    #11

--- src/store/use-cart-store.ts
+++ src/store/use-cart-store.ts
@@ -1,7 +1,7 @@
 import type { CartState } from '@/types/cart.model';
+import toast from 'react-hot-toast';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-
 
 export const useCartStore = create<CartState>()(
   persist(
@@ -10,12 +10,21 @@ export const useCartStore = create<CartState>()(
       addToCart: (item) => {
         const existingItem = get().cartItems.find((i) => i.productId === item.productId);
         if (existingItem) {
+          const totalQuantity = existingItem.quantity + item.quantity;
+          if (totalQuantity > item.countInStock) {
+            toast.error('موجودی کافی نیست');
+            return;
+          }
           set({
             cartItems: get().cartItems.map((i) =>
-              i.productId === item.productId ? { ...i, quantity: i.quantity + item.quantity } : i
+              i.productId === item.productId ? { ...i, quantity: totalQuantity } : i
             ),
           });
         } else {
+          if (item.quantity > item.countInStock) {
+            toast.error('موجودی کافی نیست');
+            return;
+          }
           set({ cartItems: [...get().cartItems, item] });
         }
       },
@@ -23,16 +32,25 @@ export const useCartStore = create<CartState>()(
         set({
           cartItems: get().cartItems.filter((item) => item.productId !== productId),
         }),
-      updateQuantity: (productId, quantity) =>
+      updateQuantity: (productId, quantity) => {
+        const item = get().cartItems.find((i) => i.productId === productId);
+        if (!item) return;
+
+        if (quantity > item.countInStock) {
+          toast.error('تعداد بیشتر از موجودی است');
+          return;
+        }
+
         set({
-          cartItems: get().cartItems.map((item) =>
-            item.productId === productId ? { ...item, quantity } : item
+          cartItems: get().cartItems.map((i) =>
+            i.productId === productId ? { ...i, quantity } : i
           ),
-        }),
-        clearCart: () => set({cartItems: []}),
+        });
+      },
+      clearCart: () => set({ cartItems: [] }),
     }),
     {
-      name: 'cart-storage', 
+      name: 'cart-storage',
     }
   )
 );
