6a9495c1230175a4dd8b5c24443dc041a1ebcb78
ZahraKiani <zahra.kiani7776@gmail.com>
2025-07-07 23:11:17 +0330

    feat:
    #7

--- src/components/CommentSubmit.tsx
+++ src/components/CommentSubmit.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Select,
   SelectContent,
@@ -8,26 +8,74 @@ import {
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import type { ProductModel } from '@/types/product.model';
+import toast from 'react-hot-toast';
+import { useMutation } from '@tanstack/react-query';
+import { axiosInstance } from '@/lib/utils';
 
-const CommentSubmit = () => {
+const CommentSubmit = ({ product }: { product: ProductModel }) => {
+  const [rating, setRating] = useState(1);
+  const [comment, setComment] = useState('');
+
+  const createReview = useMutation({
+    mutationFn: async ({ rating, comment }) => {
+      try {
+        const res = await axiosInstance.post(`/products/${product._id}/reviews`, {
+          rating,
+          comment,
+        });
+        return res.data;
+      } catch (error) {
+        console.error('error:', error.response || error.message);
+        throw error;
+      }
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createReview.mutate(
+      { rating, comment },
+      {
+        onSuccess: () => {
+          setRating(1);
+          setComment('');
+          toast.success('نظر با موفقیت ثبت شد');
+        },
+        onError: (error) => {
+          toast.error(error.message);
+        },
+      }
+    );
+  };
+  function handleRatingClick(value: number) {
+    setRating(Number(value));
+  }
+
   return (
-    <div className="flex flex-col gap-4 w-[700px]">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-[700px]">
       <label className="font-medium">امتیاز</label>
-      <Select>
+      <Select onValueChange={handleRatingClick}>
         <SelectTrigger className="w-[700px] bg-white">
           <SelectValue placeholder="انتخاب امتیاز" />
         </SelectTrigger>
         <SelectContent className="bg-white">
-          <SelectItem value="light">عالی</SelectItem>
-          <SelectItem value="system">خوب</SelectItem>
-          <SelectItem value="system">متوسط</SelectItem>
-          <SelectItem value="system">ضعیف</SelectItem>
+          <SelectItem value="5">عالی</SelectItem>
+          <SelectItem value="4">خیلی خوب</SelectItem>
+          <SelectItem value="3">خوب</SelectItem>
+          <SelectItem value="2">متوسط</SelectItem>
+          <SelectItem value="1">ضعیف</SelectItem>
         </SelectContent>
       </Select>
       <label className="font-medium">نظر</label>
-      <Textarea placeholder="نظر خود را وارد نمایید" className="w-[700px] bg-white" />
-      <Button size={'sm'}>ثبت نظر</Button>
-    </div>
+      <Textarea
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        placeholder="نظر خود را وارد نمایید"
+        className="w-[700px] bg-white"
+      />
+      <Button size={'lg'}> {createReview.isPending ? 'در حال ارسال...' : 'ثبت نظر'}</Button>
+    </form>
   );
 };
 
