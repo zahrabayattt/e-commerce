856bd41f43c89bfc725f3b20fc6b18f99bfbc2a4
javadi <javadi89s@gmail.com>
2025-07-07 19:49:41 +0330

    refactor: :art: replaced dashboard with Home on user page
    #1

--- src/components/NavbarItems.tsx
+++ src/components/NavbarItems.tsx
@@ -10,9 +10,12 @@ import {
   UserCog,
 } from 'lucide-react';
 
-
 export const NavbarItems = (isAdmin: boolean): NavItem[] => [
-  { title: 'داشبورد', address: isAdmin ? 'dashboard' : '/', icon: <House size={20} /> },
+  {
+    title: isAdmin ? 'داشبورد' : 'صفحه اصلی',
+    address: isAdmin ? 'dashboard' : '/',
+    icon: <House size={20} />,
+  },
   { title: 'فروشگاه', address: 'shop', icon: <ShoppingBag size={20} /> },
   { title: 'سبد خرید', address: 'cart', icon: <ShoppingCart size={20} /> },
   { title: 'علاقه‌مندی‌ها', address: 'favorite', icon: <Heart size={20} /> },
