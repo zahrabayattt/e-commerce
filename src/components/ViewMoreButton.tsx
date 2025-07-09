a1889b9b1f6bf1ffc64ee11fb019deb6193a482a
Zahra <zahrabayat367@gmail.com>
2025-07-09 21:47:47 +0330

    feat: :bug: update redirect in AllProducts page

--- src/components/ViewMoreButton.tsx
+++ src/components/ViewMoreButton.tsx
@@ -1,20 +1,17 @@
-import { ArrowLeft } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 interface ViewMoreButtonProps {
   onClick?: () => void;
   text?: string;
-  _id:string;
+  _id: string;
 }
 
-export default function ViewMoreButton({
-  text = "مشاهده بیشتر",
-  _id,
-}: ViewMoreButtonProps) {
-    const navigate = useNavigate()
+export default function ViewMoreButton({ text = 'مشاهده بیشتر', _id }: ViewMoreButtonProps) {
+  const navigate = useNavigate();
   return (
     <button
-      onClick={() => navigate(_id)}
+      onClick={() => navigate(`/product/${_id}`)}
       className="flex items-center gap-2 px-3 py-2 bg-[#DB2777] text-white rounded-md hover:bg-[#871849] transition-colors text-sm cursor-pointer"
     >
       {text}
