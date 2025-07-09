6868072ba03adb8dcb0c4c296a706afde7378a6f
moazame <moazame.abdolrezaei80@gmail.com>
2025-07-09 12:40:04 +0330

    fix: :bug: edit checkout page

--- src/components/OrderItemRow.tsx
+++ src/components/OrderItemRow.tsx
@@ -11,7 +11,9 @@ const OrderItemRow = ({ item }: { item: IOrderItem }) => {
           className="w-10 h-10 object-cover"
         />
       </TableCell>
-      <TableCell className="text-right">{item.name}</TableCell>
+      <TableCell className="text-right max-w-[200px] truncate whitespace-nowrap overflow-hidden">
+        {item.name}
+      </TableCell>
       <TableCell className="text-center">{item.qty}</TableCell>
       <TableCell className="text-center">{item.price.toLocaleString()}</TableCell>
       <TableCell className="text-center">{(item.price * item.qty).toLocaleString()}</TableCell>
